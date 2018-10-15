8543141787a89059679c7415cac65c8d92f5f5a8
Booldook <booldook@gmail.com>
2018-10-15 17:17:18 +0900

    181015-03

--- js/main.js
+++ js/main.js
@@ -29,6 +29,88 @@ $("#bt_weekly").click(function(){
 		$("#bt_weekly").removeClass("tab_disable").addClass("tab_active");
 	}
 });
+$("#modal_open").click(function(){
+	$("#modal").show();
+});
+$("#modal_close").click(function(){
+	$("#modal").hide();
+});
+$.ajax({
+	url: "../json/city.json",
+	type: "get",
+	dataType: "json",
+	success: function(data){
+		var city = data.cities;
+		var html = '';
+		for(var i=0; i<city.length; i++) {
+			html = '<option value="'+city[i].id+'">'+city[i].name+'</option>';
+			$("#area").append(html);
+		}
+	},
+	error:function(xhr, status, error) {
+		console.log(xhr, status, error);
+	}
+});
+$("#area").change(function(){
+	var id = $(this).val();
+	var city = $(this).find('option:selected').text();
+	var appid = "02efdd64bdc14b279bc91d9247db4722";
+	var units = "metric";
+	var dt = new Date();
+	var date = dt.getFullYear()+"년 "+(dt.getMonth()+1)+"월 "+dt.getDate()+"일";
+	$.ajax({
+		url: "https://api.openweathermap.org/data/2.5/weather",
+		type: "get",
+		dataType: "json",
+		data: {
+			id: id,
+			appid: appid,
+			units: units
+		},
+		success: function(data){
+			$(".dl_icon").attr("src", "../img/icon/"+data.weather[0].icon+".png");
+			$(".dl_area > span").html(city);
+			$(".dl_date").html(date);
+			$(".dl_temp").html(data.main.temp+'℃ (최고:'+data.main.temp_max+'℃ / 최저:'+data.main.temp_min+'℃)');
+			$(".dl_desc").html(data.weather[0].description);
+			$("#modal").hide();
+		},
+		error: function(xhr, status, error) {
+			console.log(xhr, status, error);
+		}
+	});
+	$.ajax({
+		url: "https://api.openweathermap.org/data/2.5/forecast",
+		type: "get",
+		dataType: "json",
+		data: {
+			id: id,
+			appid: appid,
+			units: units
+		},
+		success: function(data){
+			$(".weekly").empty();
+			for(var i=0; i<data.cnt; i++) {
+				html = '<li class="clear">';
+				html+= '<img src="../img/icon/'+data.list[i].weather[0].icon+'.png" class="img wk_icon">';
+				html+= '<ul>';
+				html+= '<li class="wk_area">'+city+'</li>';
+				html+= '<li class="wk_date">'+data.list[i].dt_txt+'</li>';
+				html+= '<li class="wk_temp">'+data.list[i].main.temp+'℃</li>';
+				html+= '<li class="wk_desc">'+data.list[i].weather[0].description+'</li>';
+				html+= '</ul>';
+				html+= '</li>';
+				$(".weekly").append(html);
+			}
+		},
+		error: function(xhr, status, error) {
+			console.log(xhr, status, error);
+		}
+	});
+});
+
+
+
 
 /*
 $(".tabs > li").click(function(){
@@ -51,5 +133,8 @@ $(".tabs > li").click(function(){
 	}
 });
 */
-
+/*
+https://api.openweathermap.org/data/2.5/weather?id=1835848&appid=02efdd64bdc14b279bc91d9247db4722&units=metric
+https://api.openweathermap.org/data/2.5/forecast?id=1835848&appid=02efdd64bdc14b279bc91d9247db4722&units=metric
+*/
 

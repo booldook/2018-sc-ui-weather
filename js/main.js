37f0ffab8b19c46bf0d2bc98e8747495fb63e49a
Booldook <booldook@gmail.com>
2018-10-15 13:56:17 +0900

    181015-03

--- js/main.js
+++ js/main.js
@@ -8,9 +8,48 @@ $(window).resize(function(){
 	else {
 		//PC
 		$(".tabs > li").removeClass("tab_active tab_disable");
+		$(".daily").show();
+		$(".weekly").show();
 	}
 }).trigger("resize");
 
-$(".tabs > .tab_disable").click(function(){
-	
+$("#bt_daily").click(function(){
+	if($(window).width() < 800) {
+		$(".daily").show();
+		$(".weekly").hide();
+		$("#bt_daily").removeClass("tab_disable").addClass("tab_active");
+		$("#bt_weekly").removeClass("tab_active").addClass("tab_disable");
+	}
 });
+$("#bt_weekly").click(function(){
+	if($(window).width() < 800) {
+		$(".daily").hide();
+		$(".weekly").show();
+		$("#bt_daily").removeClass("tab_active").addClass("tab_disable");
+		$("#bt_weekly").removeClass("tab_disable").addClass("tab_active");
+	}
+});
+
+/*
+$(".tabs > li").click(function(){
+	$(".tabs > li").removeClass("tab_active tab_disable");
+	$(this).addClass("tab_active");
+	$(this).siblings().addClass("tab_disable");
+	if($(this).index() == 0) {
+		//daily
+		//$(".tabs > li").eq(0).addClass("tab_active");
+		//$(".tabs > li").eq(1).addClass("tab_disable");
+		$(".daily").show();
+		$(".weekly").hide();
+	}
+	else {
+		//weekly
+		//$(".tabs > li").eq(0).addClass("tab_disable");
+		//$(".tabs > li").eq(1).addClass("tab_active");
+		$(".daily").hide();
+		$(".weekly").show();
+	}
+});
+*/
+
+
